df815b4a4d9f46dfd2212bea82e4407542450195
ecmnet <ecm@gmx.de>
2021-06-10 16:08:51 +0200

    Reduce waiting time

--- mavutils/src/main/java/com/comino/mavutils/hw/jetson/CycleUSBHub.java
+++ mavutils/src/main/java/com/comino/mavutils/hw/jetson/CycleUSBHub.java
@@ -1,7 +1,7 @@
 package com.comino.mavutils.hw.jetson;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+
 import java.io.InputStreamReader;
 
 public class CycleUSBHub {
@@ -16,7 +16,7 @@ public class CycleUSBHub {
 				System.out.println(line);
 			}
 			reader.close();
-			Thread.sleep(5000);
+			Thread.sleep(2000);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
